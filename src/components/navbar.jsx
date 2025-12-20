55136476dc97d6529366d60f32f5c4d0d4ad27ae
asionsolver <asionsolver274@gmail.com>
2025-12-20 17:22:36 +0600

    Section: Navbar component complete

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,35 @@
+import { images } from "../assets/assets";
+
 const NavBar = () => {
-  return <div className="p-5 bg-blue-500 text-2xl text-white">NavBar</div>;
+  return (
+    <div className="py-5 bg-prime-white text-prime-brown">
+      <div className="px-4 centered-row justify-between">
+        {/* Logo */}
+        <div className="">
+          <img src={images.logo} alt="logo" className="" />
+        </div>
+        {/* Links: In Desktop Only */}
+        <ul className="hidden md:centered-row gap-4 lg:gap-10 font-medium cursor-pointer">
+          {["About", "Subscribe", "Episode", "Insider"].map((item, index) => {
+            return (
+              <li
+                key={index}
+                className="hover:font-semibold hover:-translate-y-1 transition-all duration-200 ease-out"
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        {/* Menu Icon: In Mobile Only */}
+        <img src={images.menu} alt="menu_icon" className="md:hidden" />
+        {/* Subscribe */}
+        <div className="hidden md:block px-5 py-3 rounded-full border border-prime-brown cursor-pointer hover:bg-prime-brown hover:text-zinc-50 hover:-translate-y-1 transition-all duration-300 ease-out hover:shadow-xl  hover:shadow-amber-100">
+          <span className="font-semibold">Subscribe Now</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
