a999a6cc662a217a06eebaf159a75bd27b44a9d5
asionsolver <asionsolver274@gmail.com>
2025-12-22 19:35:07 +0600

    fix responsive issue in message and footer section

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -8,15 +8,15 @@ const Footer = () => {
         <img
           src={images.logo}
           alt="logo"
-          className="my-8 w-50 cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-out"
+          className="my-8 w-50 cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-out md:w-60 lg:w-64"
         />
         {/* Grid */}
-        <div className="w-full grid grid-cols-2 gap-4 cursor-pointer">
+        <div className="w-full grid grid-cols-2 gap-4 cursor-pointer md:grid-cols-4 lg:px-10">
           {["About", "Subscribe", "Episode", "Insider"].map((item, index) => {
             return (
               <span
                 key={index}
-                className="font-semibold transition2 hover:-translate-y-1"
+                className="font-semibold transition2 hover:-translate-y-1 md:text-lg lg:text-xl"
               >
                 {item}
               </span>
@@ -26,25 +26,25 @@ const Footer = () => {
         {/* Horizontal Line */}
         <hr className="text-prime-brown/70 my-10" />
         {/* Socials */}
-        <div className="grid grid-cols-2 gap-4 px-2">
+        <div className="grid grid-cols-2 gap-4 px-2 md:centered-row md:justify-between md:px-24 lg:px-90">
           {socialLinks.map(({ _id, name, icon }) => {
             return (
               <button
                 key={_id}
-                className="w-fit h-10 p-6 border border-prime-brown/80 centered-row gap-2 rounded-full"
+                className="w-fit h-10 p-6 border border-prime-brown/80 centered-row gap-2 rounded-full hover:shadow-2xl  hover:bg-amber-300 hover:shadow-amber-100 cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-out"
               >
                 {/* Icon */}
                 <div className="w-6 h-6">
                   <img src={icon} alt="icon" />
                 </div>
                 {/* .Label */}
-                <span className="text-prime-brown clash-display">{name}</span>
+                <span className="text-prime-brown clash-display ">{name}</span>
               </button>
             );
           })}
         </div>
         {/* Copy right note */}
-        <p className="mt-14 text-sm text-prime-brown text-center">
+        <p className="mt-14 text-sm text-prime-brown text-center md:text-base lg:text-lg">
           &copy; Asionsolver {new Date().getFullYear()}, All rights reserved.
         </p>
       </div>
--- src/sections/message.jsx
+++ src/sections/message.jsx
@@ -2,8 +2,8 @@ import {} from "../assets/assets";
 
 const Message = () => {
   return (
-    <div className="py-5">
-      <div className="px-8 text-center center-item py-4  text-prime-brown text-3xl">
+    <div className="py-5 bg-white lg:py-20">
+      <div className="px-8  py-4 text-center center-item  text-prime-brown text-3xl md:text-4xl lg:text-6xl lg:px-50">
         <p className="clash-display">
           Unlock invaluable wisdom personalized for{" "}
           <span className="text-prime-brown/50 clash-display">
