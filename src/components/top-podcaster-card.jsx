060dfb7cdbd76c39f05abec4606b9408d054b8ec
asionsolver <asionsolver274@gmail.com>
2025-12-22 17:01:06 +0600

    fix responsive issue for top podcast  section

--- src/components/top-podcaster-card.jsx
+++ src/components/top-podcaster-card.jsx
@@ -7,18 +7,18 @@ const TopPodcasterCard = ({ bgColor = "#ff854b" }) => {
         backgroundColor: bgColor,
         color: bgColor === "#ffd25a" ? "#532822" : "#f4f4f5",
       }}
-      className="min-w-full  h-full relative pt-8 px-6 rounded-3xl overflow-clip"
+      className="min-w-full md:min-w-1/2  h-full relative pt-8 px-6 rounded-3xl overflow-clip lg:min-w-125 lg:py-16 lg:px-8 cursor-pointer"
     >
       {/* Image */}
       <img
         src={images.week_01}
         alt="image"
-        className="w-[70%] absolute bottom-0 right-0"
+        className="w-[50%] md:w-[70%] absolute bottom-0 right-0 "
       />
       {/* Title */}
       <div className="col gap-3">
-        <h2 className="text-4xl  clash-display">Mr.James</h2>
-        <span className="text-prime-brown bg-zinc-100 p-2 px-4 rounded-full text-sm w-fit">
+        <h2 className="text-4xl  clash-display lg:text-5xl">Mr.James</h2>
+        <span className="text-prime-brown bg-zinc-100 p-2 px-4 rounded-full text-sm w-fit lg:mt-2 lg:text-xl">
           1.6 million streams
         </span>
       </div>
@@ -27,7 +27,7 @@ const TopPodcasterCard = ({ bgColor = "#ff854b" }) => {
       <img
         src={images.decor}
         alt="decorative"
-        className="w-20 absolute bottom-4 left-2"
+        className="w-20 absolute bottom-4 left-2 lg:left-6 lg:bottom-8"
       />
     </div>
   );
--- src/sections/top-podcast.jsx
+++ src/sections/top-podcast.jsx
@@ -3,15 +3,15 @@ import TopPodcasterCard from "../components/top-podcaster-card";
 
 const TopPodcast = () => {
   return (
-    <div className="py-5 bg-white">
+    <div className="py-5 bg-white lg:mt-5">
       <div className="px-8 pb-2 ">
         {/* Header */}
-        <h2 className="text-3xl text-center text-prime-brown clash-display">
+        <h2 className="text-3xl text-center text-prime-brown clash-display md:text-4xl lg:text-5xl">
           Top Podcasters of the Week
         </h2>
 
         {/* Cards Section */}
-        <div className="my-10 w-full h-82.5 whitespace-nowrap centered-row gap-4 overflow-x-scroll ">
+        <div className="my-10 w-full h-82.5 whitespace-nowrap centered-row gap-4 overflow-x-scroll lg:h-130">
           {/* Card */}
           <TopPodcasterCard bgColor="#532822" />
           <TopPodcasterCard bgColor="#43a574" />
